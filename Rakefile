d86992e1b2e157bb428f720d27f6933fe2c91046
raggi <jftucker@gmail.com>
2009-10-30 14:59:04 +0000

    Initial commit

---
+++ Rakefile
@@ -0,0 +1,14 @@
+#!/usr/bin/env rake
+
+require 'hoe'
+Hoe.plugin :doofus, :git
+
+Hoe.spec 'vlad-daemon_kit' do
+  developer('James Tucker', 'raggi@rubyforge.org')
+  
+  self.extra_rdoc_files = FileList["*.rdoc"]
+  self.history_file     = "CHANGELOG.rdoc"
+  self.readme_file      = "README.rdoc"
+  self.rubyforge_name   = "libraggi"
+  self.testlib          = :minitest
+end
---
+++ lib/vlad/daemon_kit.rb
@@ -0,0 +1,23 @@
+require 'vlad'
+
+class Vlad::DaemonKit
+  VERSION = '1.0.0'
+
+  set :mkdirs, 'tmp'
+  set :shared_paths, %w(log)
+
+  namespace :vlad do
+    desc "Start the daemon"
+    remote_task :start_app, :roles => :app do
+      commands = %w(stop start).map do |command|
+        "ruby #{latest_release}/bin/#{application} #{c}"
+      end
+      run commands.join(' && ')
+    end
+
+    desc "Stop the daemon"
+    remote_task :stop_app, :roles => :app do
+      run "ruby #{latest_release}/bin/#{application} stop"
+    end
+  end
+end
