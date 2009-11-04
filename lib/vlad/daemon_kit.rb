5c16d755834e0bf17d68ff276d9091558a630b1f
raggi <jftucker@gmail.com>
2009-11-04 13:57:29 +0000

    1.0.1, support different environments, and default to production.

--- lib/vlad/daemon_kit.rb
+++ lib/vlad/daemon_kit.rb
@@ -1,23 +1,24 @@
 require 'vlad'
 
 class Vlad::DaemonKit
-  VERSION = '1.0.0'
+  VERSION = '1.0.1'
 
   set :mkdirs, 'tmp'
   set :shared_paths, { 'log' => 'log' }
+  set :environment, 'production'
 
   namespace :vlad do
     desc "Start the daemon"
     remote_task :start_app, :roles => :app do
       commands = %w(stop start).map do |command|
-        "ruby #{current_path}/bin/#{application} #{command}"
+        "ruby #{current_path}/bin/#{application} -e #{environment} #{command}"
       end
       run commands.join(' && ')
     end
 
     desc "Stop the daemon"
     remote_task :stop_app, :roles => :app do
-      run "ruby #{current_path}/bin/#{application} stop"
+      run "ruby #{current_path}/bin/#{application} -e #{environment} stop"
     end
   end
 end
